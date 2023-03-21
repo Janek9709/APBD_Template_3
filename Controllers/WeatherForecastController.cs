32276e05ef4b6ea56a2a1ccfc8699a2bd4f9d1f4
Janek9709 <jan.biniek97@gmail.com>
2023-03-21 14:56:33 +0100

    Remove unnecessary

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -9,25 +9,17 @@ namespace Zadanie3.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var list = new List<WeatherForecast>();
+            foreach (var item in Summaries)
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                list.Add(new WeatherForecast { Date = new DateOnly(2015, 12, 31), Summary = item, TemperatureC = -1 });
+            }
+            return list;
         }
     }
 }
